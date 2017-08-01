b9f194f8e410a4c557d9ba81f3a22a25cf0b6f95
tangloner <tangloner@gmail.com>
2017-08-01 17:00:40 +0800

    script

--- ps.sh
+++ ps.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 
-if [ "$1" = "ps" ]
-then
+python ./example.py $1 $2 --job_name=$3 --task_index=$4
 
-python /root/code/LogicRgression/offline_logistic_regression.py --job_name=$1 --task_index=$2 --batch_size=500 --num_epochs=1 --train=/root/data/url_svmlight/Day#.svm --line_skip_count=0 --features=3231961
-
-elif [ "$1" = "worker" ]
-then
-
-python /root/code/LogicRgression/offline_logistic_regression.py --job_name=$1 --task_index=$2 --btach_size=500 --num_epochs=1 --train=/root/data/url_svmlight/Day#.svm --line_skip_count=0 --features=3231961 --data_index_start=$3 --data_index_end=$4
-
-fi
